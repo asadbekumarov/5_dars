f6637c060c3439081cd3a2bb45d247ae5274f3e4
asadbekumarov <asadbekumarov922@gmail.com>
2024-09-01 02:25:14 +0500

    end

---
+++ script.js
@@ -0,0 +1,174 @@
+// 1-masala
+// let k = 5;
+// let n = 3;
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+// 2-masala
+// let a = 10;
+// let b = 20;
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+//   sum += 1;
+// }
+// console.log(`jami sonlar ${sum}`);
+
+// 3-masala
+// let a = 40;
+// let b = 30;
+// let sum = 0;
+// for (let i = 39; i > b; i--) {
+//   console.log(i);
+//   sum += 1;
+// }
+// console.log(`jami sinlar ${sum}`);
+
+// 4-masala
+// let a = 10000;
+// for (let i = 1; i <= 10; i++) {
+//   let umumiyNarx = i * a;
+//   console.log(`${i} br kg narxi ${umumiyNarx} hammasi`);
+// }
+
+// 5-masala
+// let a = 10000;
+// for (let i = 0; i <= 1; i++) {
+//   let umumiyNarx = i * a;
+//   console.log(`${i}  kg narxi   hammasi ${umumiyNarx} `);
+// }
+
+// 6-masala
+// let a = 10000;
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//   let umumiyNarx = i * a;
+//   console.log(`${i} kg narxi ${umumiyNarx} hammasi`);
+// }
+
+// 7-masala
+// let a = 10;
+// let b = 15;
+// let sum = 0;
+// for (let i = a; i < b; i++) {
+//   sum = sum += a;
+// }
+// console.log(`a dan b gaca bolgan son yigndisi ${sum}`);
+
+// 8-masala
+// let a = 2;
+// let b = 3;
+// let sum = 1;
+// for (let i = a; i <= b; i++) {
+//   sum *= i;
+// }
+// console.log(`a dan b gaca bolgan son yigndisi ${sum}`);
+
+// 9-masala
+// var a = 1;
+// var b = 5;
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   sum += i * i;
+// }
+// console.log(`Kvadratlar yig'indisi ${sum}`);
+
+// 10-masala
+// let n = 5;
+// let s = 0;
+// for (let i = 1; i <= n; i++) {
+//   s += 1 / i;
+//   console.log(s);
+// }
+
+// WHILE
+// 1-masla
+// {
+//   let a = 20;
+//   let b = 10;
+//   while (a > b) {
+//     a -= b;
+//     console.log(a);
+//   }
+// }
+
+// 2-masala
+// {
+//   let a = 10;
+//   let b = 3;
+//   let count = 0;
+//   let temp = a;
+//   while (temp >= b) {
+//     temp -= b;
+//     count++;
+//     console.log(count);
+//   }
+// }
+
+// 3-masala
+// let n = 29;
+// let k = 5;
+// let butunQismi = 0;
+// let qoldiq = n;
+// while (qoldiq >= k) {
+//   qoldiq = qoldiq - k;
+//   butunQismi++;
+// }
+// console.log(`butun qismi ${butunQismi}`);
+// console.log(`qoldiq ${qoldiq}`);
+
+// 5-masala
+// {
+//   let n = 16;
+//   let k = 0;
+//   while (n > 1) {
+//     n = n / 2;
+//     k++;
+//     console.log(k);
+//   }
+// }
+
+// 6-masala
+// {
+//   let n = 5;
+//   let result = 1;
+//   while (n > 0) {
+//     result *= n;
+//     n -= 2;
+//     console.log(result);
+//   }
+// }
+
+// 7-masala
+// let n = 10;
+// let k = 1;
+// while (k * k <= n) {
+//   k++;
+// }
+// console.log(k);
+
+// 8-masala
+// let n = 100;
+// let k = 20;
+// while ((k + 1) * (k + 1) <= n) {
+//   k++;
+// }
+// console.log(k);
+
+// 9-masala
+// let n = 100;
+// let k = 0;
+// while (3 ** k <= n) {
+//   k++;
+// }
+// console.log(k);
+
+// 10-masala
+// let n = 1455421;
+// let k = 5;
+// let c = 1;
+// while (c * 3 <= n) {
+//   c *= 3;
+//   k++;
+// }
+// console.log(k);
